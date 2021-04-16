390ae5b58198a50dbd1c62af30a264ccbfb5ea70
Phy3hogga <alexander.hogg@ntu.ac.uk>
2021-04-16 19:27:04 +0100

    Updated Floating_Point_Equal to support Tall Arrays

--- Floating_Point_Equal.m
+++ Floating_Point_Equal.m
@@ -25,42 +25,71 @@ function yn = Floating_Point_Equal(A, B)
 %
 % See also: EPS, ISEQUAL
 
-%% Check arguments
-narginchk(2,2);
-assert(isfloat(A) && isfloat(B), 'inputs a and b must be floats');
+    %% Check arguments
+    narginchk(2,2);
+    if(~(Validate_Floating_Point_Equal_Input(A) && Validate_Floating_Point_Equal_Input(B)))
+        error(strcat("Floating_Point_Equal : Expected input ", Variable_Name, " to be a float."));
+    end
+    %assert(isfloat(A) && isfloat(B), 'inputs a and b must be floats');
 
-%Ensure the length of Dimensions_A and Dimensions_B match
-Dimensions_A = size(A);
-Dimensions_B = size(B);
-if(length(Dimensions_A) == length(Dimensions_B))
-    %Continue
-else
-    %Add '1' to missing dimensions for elementwise comparison
-    Dimension_List = [length(Dimensions_A), length(Dimensions_B)];
-    for Current_Dimension = min(Dimension_List):max(Dimension_List)
-        if(Dimension_List(1) > Dimension_List(2))
-            Dimensions_B(Current_Dimension) = 1;
-        else
-            Dimensions_A(Current_Dimension) = 1;
+    %Ensure the length of Dimensions_A and Dimensions_B match
+    Dimensions_A = size(A);
+    if(istall(A))
+        Dimensions_A = gather(Dimensions_A);
+    end
+    Dimensions_B = size(B);
+    if(istall(B))
+        Dimensions_B = gather(Dimensions_B);
+    end
+    
+    if(length(Dimensions_A) == length(Dimensions_B))
+        %Continue
+    else
+        %Add '1' to missing dimensions for elementwise comparison
+        Dimension_List = [length(Dimensions_A), length(Dimensions_B)];
+        for Current_Dimension = min(Dimension_List):max(Dimension_List)
+            if(Dimension_List(1) > Dimension_List(2))
+                Dimensions_B(Current_Dimension) = 1;
+            else
+                Dimensions_A(Current_Dimension) = 1;
+            end
         end
     end
-end
-Size_A = numel(A);
-Size_B = numel(B);
-if(Dimensions_A == Dimensions_B)
-    %Identical sizes already
-elseif((Size_A == 1) || (Size_B == 1))
-    if(Size_A == 1)
-        A = repmat(A, size(B));
-    elseif(Size_B == 1)
-        B = repmat(B, size(A));
+    
+    %Get number of elements in the array
+    Size_A = prod(Dimensions_A);
+    Size_B = prod(Dimensions_B);
+
+    if(Dimensions_A == Dimensions_B)
+        %Identical sizes already
+    elseif((Size_A == 1) || (Size_B == 1))
+        if(Size_A == 1)
+            %A = repmat(A, Dimensions_B);
+        elseif(Size_B == 1)
+            %B = repmat(B, Dimensions_A);
+        else
+            assert(all(size(A) == size(B)), 'Unknown input condition');
+        end
     else
-        assert(all(size(A) == size(B)), 'Unknown input condition');
+        assert(all(size(A) == size(B)), 'inputs a and b must be the same size');
     end
-else
-    assert(all(size(A) == size(B)), 'inputs a and b must be the same size');
+
+    %% Check for equivalence of each element, within the tolerance
+    yn = abs(A - B) <= eps(max(abs(A), abs(B)));
+    %yn = all(yn);  % scalar logical output
 end
 
-%% Check for equivalence of each element, within the tolerance
-yn = abs(A - B) <= eps(max(abs(A), abs(B)));
-%yn = all(yn);  % scalar logical output
+%% Validate tall arrays as well as standard float variables
+function Valid = Validate_Floating_Point_Equal_Input(Variable)
+    Valid = true;
+    if(isfloat(Variable))
+    elseif(istall(Variable))
+        if(~isaUnderlying(Variable, 'float'))
+            Valid = false;
+        end
+    else
+        Valid = false;
+    end
+end
+
+%% 
