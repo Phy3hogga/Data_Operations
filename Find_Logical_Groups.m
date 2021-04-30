44a402383b33c17c7aa41b3ef0b9e4f7bc8ae718
Phy3hogga <alexander.hogg@ntu.ac.uk>
2021-04-30 18:14:04 +0100

    Updated debug messages on error to include function name.

--- Find_Logical_Groups.m
+++ Find_Logical_Groups.m
@@ -2,22 +2,22 @@
 function [Group_Start, Group_End] = Find_Logical_Groups(Input_Array)
     %% Input Handling
     if(nargin ~= 1)
-        error("Find Logical Groups : Expected single logical array as input");
+        error("Find_Logical_Groups : Expected single logical array as input");
     end
     if(~islogical(Input_Array))
-        error("Find Logical Groups : Expected logical array as input");
+        error("Find_Logical_Groups : Expected logical array as input");
     end
     %% Validate array is the correct orientation
     Array_Size = size(Input_Array);
     if(numel(Array_Size) > 2)
-        error("Expected one dimensional array.");
+        error("Find_Logical_Groups : Expected one dimensional array.");
     end
     
     %% If the array requires flipping or not
     Flip_Array = false;
     if(Array_Size(1) == 1)
         if(Array_Size(2) == 1)
-            error("Expected one dimensional array wtih more than one element.");
+            error("Find_Logical_Groups : Expected one dimensional array wtih more than one element.");
         else
             Flip_Array = true;
         end
@@ -25,7 +25,7 @@ function [Group_Start, Group_End] = Find_Logical_Groups(Input_Array)
         if(Array_Size(2) == 1)
             %No flip required
         else
-            error("Expected one dimensional array.");
+            error("Find_Logical_Groups : Expected one dimensional array.");
         end
     end
     %% Flip array to be linear list
@@ -42,6 +42,6 @@ function [Group_Start, Group_End] = Find_Logical_Groups(Input_Array)
     Group_End = strfind(Input_Array', [1, 0]) - 1;
     %% Sense-Check Output
     if(length(Group_Start) ~= length(Group_End))
-        warning("Find Logical Grouping : Unexpected error, differing lengths for Start and End.");
+        warning("Find_Logical_Groups : Unexpected error, differing lengths for Start and End.");
     end
 end
