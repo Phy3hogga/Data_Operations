c117ba7b6b46184aa032c9ca3ba251f0f86872c5
Phy3hogga <alexander.hogg@ntu.ac.uk>
2022-03-18 14:40:49 +0000

    Overhauled figure hiding/unhiding functionality

--- Get_Figure.m
+++ Get_Figure.m
@@ -3,12 +3,8 @@
 % - Figure_ID (Handle) ; Handle reference to the figure window
 % - Hide_Figure (boolean) ; If the figure should be hidden or not
 function Figure_ID = Get_Figure(Figure_ID, Hide_Figure)
-    %No figure identification
-    if(nargin == 0)
-        %% No inputs specified; Figure existance can't be verified - create a new figure
-        Figure_ID = figure();
     %Only figure ID parsed
-    elseif(nargin == 1)
+    if(nargin == 1)
         %% Check if figure exists
         if(isempty(Figure_ID))
             %% Figure doesn't exist
@@ -22,29 +18,43 @@ function Figure_ID = Get_Figure(Figure_ID, Hide_Figure)
             %% Figure exists
             %Select figure
             set(0, 'CurrentFigure', Figure_ID);
+        end
+    %Figure ID and hide argument stated
+    elseif(nargin == 2)
+        %% Check if figure exists
+        if(isempty(Figure_ID))
+            %% Figure doesn't exist
+            %create figure
+            Figure_ID = figure();
+            %if hiding graphs from user
+            if(Hide_Figure)
+                set(Figure_ID,'visible','off');
+            else
+                set(Figure_ID,'visible','on');
+            end
+        elseif(~ishandle(Figure_ID))
+            %% Figure doesn't exist
+            %create figure
+            Figure_ID = figure();
+            %if hiding graphs from user
+            if(Hide_Figure)
+                set(Figure_ID,'visible','off');
+            else
+                set(Figure_ID,'visible','on');
+            end
+        else
+            %% Figure exists
+            %Select figure
+            set(0, 'CurrentFigure', Figure_ID);
+            %if hiding graphs from user
+            if(Hide_Figure)
+                set(Figure_ID,'visible','off');
+            else
+                set(Figure_ID,'visible','on');
+            end
         end
     else
-        %% Check if figure exists
-        if(isempty(Figure_ID))
-            %% Figure doesn't exist
-            %create figure
-            Figure_ID = figure();
-            %if hiding graphs from user
-            if(Hide_Figure)
-                set(Figure_ID,'visible','off');
-            end
-        elseif(~ishandle(Figure_ID))
-            %% Figure doesn't exist
-            %create figure
-            Figure_ID = figure();
-            %if hiding graphs from user
-            if(Hide_Figure)
-                set(Figure_ID,'visible','off');
-            end
-        else
-            %% Figure exists
-            %Select figure
-            set(0, 'CurrentFigure', Figure_ID);
-        end
+        %% No inputs specified; Figure existance can't be verified - create a new figure
+        Figure_ID = figure();
     end
 end
